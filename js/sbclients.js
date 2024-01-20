1cfec8efa95292170b6b9a7a164fa45f56bbc561
Syamsul Alam <admin@alampintar.org>
2024-01-20 23:38:07 +0700

    lali ngakses pentol

--- js/sbclients.js
+++ js/sbclients.js
@@ -1,4 +1,4 @@
-var version = "0.0.71";
+var version = "0.0.72";
 console.log("Supabase Client JS Script Version: " + version);
 
 var script = document.createElement('script');
@@ -10,7 +10,7 @@ const fetchData = async (supabase, mainDomain, columnPrefix) => {
     console.log('Fetching new data...');
     const { data, error } = await supabase
         .from(mainDomain)
-        .select(`"${columnPrefix}🧑🏻", "${columnPrefix}#️⃣", "${columnPrefix}📊", "${columnPrefix}📞", "${columnPrefix}💬", "${columnPrefix}🏷️"`);
+        .select(`"📍", "${columnPrefix}🧑🏻", "${columnPrefix}#️⃣", "${columnPrefix}📊", "${columnPrefix}📞", "${columnPrefix}💬", "${columnPrefix}🏷️"`);
 
     if (error) {
         console.error('Error fetching data:', error);
@@ -21,7 +21,7 @@ const fetchData = async (supabase, mainDomain, columnPrefix) => {
     if (!data || data.length === 0 || !data[0].hasOwnProperty(`${columnPrefix}🧑🏻`)) {
         const { data, error } = await supabase
             .from(mainDomain)
-            .select('"🧑🏻", "#️⃣", "📊", "📞", "💬", "🏷️"');
+            .select('"📍", "🧑🏻", "#️⃣", "📊", "📞", "💬", "🏷️"');
 
         if (error) {
             console.error('Error fetching data:', error);
@@ -102,7 +102,6 @@ script.onload = async function() {
     });
 
     if (row) {
-        let columnPrefix = subdomain ? subdomain.toUpperCase() : '';
         // Check if the column with the prefix exists, if not, try with a space after the prefix
         if (!row.hasOwnProperty(columnPrefix + '🧑🏻')) {
         columnPrefix += ' ';
